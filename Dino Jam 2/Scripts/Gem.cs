8bff496429cda287ddf354ea16d6315d5123d6ed
Ludivine Cormier <lilou_cormic@hotmail.com>
2023-03-05 15:45:16 -0500

    Twitchy gems

--- Dino Jam 2/Scripts/Gem.cs
+++ Dino Jam 2/Scripts/Gem.cs
@@ -2,6 +2,10 @@ using Godot;
 
 public class Gem : Area2D
 {
+    public const float Speed = 75;
+
+    private bool _clockwise = true;
+
     public void OnBodyEntered(PhysicsBody2D body)
     {
         ScoreManager.Add(1);
@@ -9,5 +13,27 @@ public class Gem : Area2D
         GemSoundPlayer.PlaySound();
 
         QueueFree();
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        if (_clockwise)
+        {
+            RotationDegrees += delta * Speed;
+
+            if (RotationDegrees > 15)
+            {
+                _clockwise = false;
+            }
+        }
+        else
+        {
+            RotationDegrees -= delta * Speed;
+
+            if (RotationDegrees < -15)
+            {
+                _clockwise = true;
+            }
+        }
     }
 }
